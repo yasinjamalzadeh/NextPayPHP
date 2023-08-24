f481920a4198611d3d67e1890d76da11c9f95709
yasinjamalzadeh <jamalzadeh.yasin@gmail.com>
2023-08-24 05:50:18 +0330

    first commit

---
+++ nextpay.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+    createNextPayPayment
+
+    In the return, We have status which when it is true, We have trans_id that we have to save it in out Database
+    and redirect user to the https://nextpay.org/nx/gateway/payment/$trans_id (replace $trans_id)
+    If the status is not true, That means we have to check error value which can be translated on this url:
+        https://nextpay.org/nx/docs#step-7
+*/
+function createNextPayPayment($api, $amount, $callbackUrl, $customer_phone = "none"){
+
+    $result = array(
+        'status' => false,
+    );
+
+    $customer_field = "";
+
+    if($customer_phone != "none"){
+        $customer_field = "&customer_phone=".$customer_phone;
+    }
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://nextpay.org/nx/gateway/token',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => 'api_key='.$api.'&amount='.$amount.'&order_id=85NX85s427'.$customer_field.'&callback_uri='.$callbackUrl,
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+
+    $response = json_decode($response);
+    if($response->code == -1 && isset($response->trans_id)){
+        $result['status'] = true;
+        $result['trans_id'] = $response->trans_id;
+    }else{
+        $result['status'] = false;
+        $result['error'] = $response->code;
+    }
+    return $result;
+}
+
+/*
+    verifyNextPayTransaction
+
+    In the return, We have status which when it is true, means that payment is successful and its done. Else, The
+    result contains error that means we have to check error value which can be translated on this url:
+        https://nextpay.org/nx/docs#step-7
+    
+    If status was true, we have card_holder which contains the customer's censored card number, Example:
+        5022-29**-****-5020
+*/
+function verifyNextPayTransaction($api, $amount, $trans_id){
+
+    $result = array(
+        'status' => false,
+    );
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://nextpay.org/nx/gateway/verify',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => 'api_key='.$api.'&amount='.$amount.'&trans_id='.$trans_id,
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+
+    $response = json_decode($response);
+    if($response->code == 0){
+        $result['status'] = true;
+        $result['card_holder'] = $response->card_holder;
+    }else{
+        $result['status'] = false;
+        $result['error'] = $response->code;
+    }
+
+    return $result;
+}
